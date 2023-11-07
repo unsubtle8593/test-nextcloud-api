14cd34e26f615c08937679fba91f0e3c349b8f9c
Belle Aerni <belleaerni@gmail.com>
2023-11-06 21:30:39 -0800

    A few minor updates for recent changes

--- src/Service.php
+++ src/Service.php
@@ -133,7 +133,7 @@ class Service
      *
      * Make sure extension is enabled before testing this event.
      *
-     * NOTE: IF you have BB_DEBUG mode set to TRUE then all events with params
+     * NOTE: IF you have DEBUG mode set to TRUE then all events with params
      * are logged to data/log/hook_*.log file. Check this file to see what
      * kind of parameters are passed to event.
      *
